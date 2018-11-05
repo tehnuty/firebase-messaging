5cdd7c92b22607866a826fe9169e799448a78471
lukak <luka.krajnc@inova.si>
2018-11-05 12:53:13 +0100

    Support for iOS notifications

--- firebase-messaging/firebase.py
+++ firebase-messaging/firebase.py
@@ -35,6 +35,7 @@ def _initialize_firebase():
     if (not len(firebase_admin._apps)):
         firebase_admin.initialize_app(cred, {"projectId": "PROJECT_ID"})
 
+
 def _get_topics_by_token(push_token):
     """
     :param push_token: required
@@ -54,6 +55,7 @@ def _get_topics_by_token(push_token):
     response_dict = json.loads(response.text)
     return response_dict['rel']['topics'].keys()
 
+
 def refresh_subscription(push_token, topics):
     """
     1. Initialize firebase
@@ -63,6 +65,11 @@ def refresh_subscription(push_token, topics):
     :param topics: tagObjs
     :return:
     """
+    # if you use multiple environments (e.g. dev,prod..)
+    _id = "environment-or-app-id"
+    if _id not in topics:
+        topics.append(_id)
+
     _initialize_firebase()
     try:
         followed_tags = _get_topics_by_token(push_token)
@@ -87,20 +94,32 @@ def refresh_subscription(push_token, topics):
     return
 
 
-def send_notification(push_token=None, push_topic=None, title=None, body=None, url=None, action=None, group_id=None, device_id=None, silent=False):
+def send_notification(send_to_all=False, push_token=None, push_topic=None, title=None, body=None, url=None, action=None, card_id=None, device_id=None, silent=False):
     """
+    :param send_to_all: optional, if send to all, set to True
     :param push_token: optional this or push_topic
     :param push_topic: optional this or push_token
     :param title: notification title
     :param body: notification body message
     :param url: deeplink
     :param action: actions (1. open app, 2. deep link, 3. silent push)
-    :param group_id: notification ID (if you want to override notification) e.g. afs:Card:001
+    :param card_id: notification ID (if you want to override notification) e.g. afs:Card:001
     :param device_id: device id of sender (so it wont silent push itself and start an infinity loop)
     :param silent: if push is silent or notification
     :return: firebase response
     """
     _initialize_firebase()
+
+    aps_alert = messaging.ApsAlert(
+                            title=title,
+                            body=body
+                        )
+    content_available = False
+    badge = 1
+    if silent:
+        content_available = True
+        badge = 0
+        aps_alert = messaging.ApsAlert()
 
     silent = str(silent)
 
@@ -109,16 +128,14 @@ def send_notification(push_token=None, push_topic=None, title=None, body=None, u
             "title": title,
             "body": body,
             "action": action,
-            "groupId": group_id,
             "silent": silent,
         }
     elif action is PushActions.OPEN_DEEP_LINK:
         payload = {
             "title": title,
             "body": body,
-            "url": url,
             "action": action,
-            "groupId": group_id,
+            "cardId": card_id,
             "silent": silent,
         }
     elif action is PushActions.SILENT_UPDATE_ACCOUNT:
@@ -127,16 +144,38 @@ def send_notification(push_token=None, push_topic=None, title=None, body=None, u
             "deviceId": device_id,
             "silent": silent
         }
-    if push_token:
+
+    apns_config = messaging.APNSConfig(
+                payload=messaging.APNSPayload(
+                    aps=messaging.Aps(
+                        alert=aps_alert,
+                        content_available=content_available,
+                        badge=badge,
+                        custom_data=payload
+                    )
+                )
+            )
+
+    if send_to_all:
         message = messaging.Message(
             android=messaging.AndroidConfig(
                 ttl=datetime.timedelta(seconds=3600),
                 priority='normal',
                 data=payload
             ),
-            token=push_topic,
+            apns=apns_config,
+            topic="environment-or-app-id",
         )
-
+    elif push_token:
+        message = messaging.Message(
+            android=messaging.AndroidConfig(
+                ttl=datetime.timedelta(seconds=3600),
+                priority='normal',
+                data=payload
+            ),
+            apns=apns_config,
+            token=push_token,
+        )
     elif push_topic:
         message = messaging.Message(
             android=messaging.AndroidConfig(
@@ -144,7 +183,8 @@ def send_notification(push_token=None, push_topic=None, title=None, body=None, u
                 priority='normal',
                 data=payload
             ),
-            topic=push_topic,
+            apns=apns_config,
+            condition="'%s' in topics && '%s' in topics" % (push_topic, "environment-or-app-id"),
         )
 
     # Send a message to devices subscribed to the combination of topics
